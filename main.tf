58556a621e89c845e6c342a89c8d3213f9245771
Michael Nye <mnye11@icloud.com>
2023-08-09 15:13:15 -0400

    Removed line in main.tf

--- main.tf
+++ main.tf
@@ -16,7 +16,6 @@ variable "aws_pem" {
 
 provider "aws" {
   region                  = var.aws_region
-  shared_credentials_file = var.aws_creds_file
   profile                 = var.aws_profile
 }
 
