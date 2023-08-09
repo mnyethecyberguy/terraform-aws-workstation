dfb9ed88c1875c87db708ed6c7c8b2bebc8d8ee9
Michael Nye <mnye11@icloud.com>
2023-08-09 15:38:13 -0400

    Added VPC and resources to main.tf

--- main.tf
+++ main.tf
@@ -40,3 +40,51 @@ data "aws_ami" "ubuntu" {
 
   owners = ["099720109477"]
 }
+
+resource "aws_vpc" "temp-vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = "true" #gives you an internal domain name
+  enable_dns_hostnames = "true" #gives you an internal host name
+  instance_tenancy     = "default"
+
+  tags = {
+    Name = "temp-vpc"
+  }
+}
+
+resource "aws_subnet" "temp-subnet" {
+  vpc_id                  = aws_vpc.temp-vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = "true" #it makes this a public subnet
+  availability_zone       = "${var.aws_region}a"
+  tags = {
+    Name = "temp-subnet"
+  }
+}
+
+resource "aws_internet_gateway" "temp-igw" {
+  vpc_id = aws_vpc.temp-vpc.id
+  tags = {
+    Name = "temp-igw"
+  }
+}
+
+resource "aws_route_table" "temp-rtble" {
+  vpc_id = aws_vpc.temp-vpc.id
+
+  route {
+    //associated subnet can reach everywhere
+    cidr_block = "0.0.0.0/0"
+    //CRT uses this IGW to reach internet
+    gateway_id = aws_internet_gateway.temp-igw.id
+  }
+
+  tags = {
+    Name = "temp-rtble"
+  }
+}
+
+resource "aws_route_table_association" "temp-rta" {
+  subnet_id      = aws_subnet.temp-subnet.id
+  route_table_id = aws_route_table.temp-rtble.id
+}
